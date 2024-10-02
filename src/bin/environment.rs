27a61dc5658e5e9b905e0f125a063feef21f9fdd
ReversedGravity <rg@youxplode.com>
2024-10-01 19:23:51 -0700

    Added sunset environment map.

--- src/bin/environment.rs
+++ src/bin/environment.rs
@@ -3,10 +3,10 @@ use glam::Vec3 as FVec3;
 use std::f32::consts::TAU;
 
 fn skylight(angle: f32) -> FVec3 {
-    let sky_color = FVec3::new(0.3, 0.7, 1.0);
-    let sun_color = FVec3::new(1.0, 1.0, 0.8) * 3.0;
+    let sky_color = FVec3::new(0.3, 0.3, 1.0) * 0.2;
+    let sun_color = FVec3::new(1.0, 0.3, 0.1) * 2.0;
     let sun_size = 0.3;
-    let sun_angle = 1.0;
+    let sun_angle = 0.6;
 
     let sun_color = if (angle - sun_angle).abs() < sun_size {
         sun_color
@@ -24,5 +24,5 @@ fn main() {
             skylight(angle)
         })
         .collect::<Vec<_>>();
-    save_env(&data, "env/default.tiff");
+    save_env(&data, "env/sunset.tiff");
 }
