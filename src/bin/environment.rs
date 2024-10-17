985e0f83b5a0ddd92056e6a4b818fb0d3d893db4
ReversedCausality <rg@youxplode.com>
2024-10-17 11:33:51 +0100

    Update colors for srgb.

--- src/bin/environment.rs
+++ src/bin/environment.rs
@@ -4,17 +4,17 @@ use std::f32::consts::TAU;
 
 fn skylight(angle: f32) -> FVec3 {
     // Default:
-    let sky_color = FVec3::new(0.3, 0.7, 1.0);
-    let sun_color = FVec3::new(1.0, 1.0, 0.8) * 5.0;
-    // Night:
-    // let sky_color = FVec3::new(0.2, 0.15, 0.6) * 0.3;
-    // let sun_color = FVec3::new(1.0, 0.3, 0.1) * 0.0;
+    // let sky_color = FVec3::new(0.3, 0.7, 1.0) * 4.0;
+    // let sun_color = FVec3::new(1.0, 1.0, 0.8) * 10.0;
+    // Sunset:
+    // let sky_color = FVec3::new(0.2, 0.15, 0.4) * 2.0;
+    // let sun_color = FVec3::new(1.0, 0.3, 0.1) * 3.0;
     // Desert:
-    // let sky_color = FVec3::new(0.6, 0.7, 1.0);
-    // let sun_color = FVec3::new(1.0, 0.9, 0.4) * 4.0;
+    // let sky_color = FVec3::new(0.6, 0.7, 1.0) * 4.0;
+    // let sun_color = FVec3::new(1.0, 0.9, 0.4) * 10.0;
     // Golden:
-    // let sky_color = FVec3::new(0.6, 0.5, 0.7);
-    // let sun_color = FVec3::new(1.0, 0.8, 0.2) * 4.0;
+    // let sky_color = FVec3::new(0.6, 0.4, 0.6) * 4.0;
+    // let sun_color = FVec3::new(1.0, 0.7, 0.0) * 10.0;
 
     let sun_size = 0.3;
     let sun_angle = 1.0;
@@ -35,5 +35,5 @@ fn main() {
             skylight(angle)
         })
         .collect::<Vec<_>>();
-    save_env(&data, "env/default.tiff");
+    save_env(&data, "env/sunset.tiff");
 }
